e5a0528bdc61ede0e650d2a546e81623c9acd170
revus05 <skninni05@gmail.com>
2026-03-20 19:42:44 +0300

    feat: add image placeholder to product page

--- garden-for-you/src/widgets/product/image/ui.tsx
+++ garden-for-you/src/widgets/product/image/ui.tsx
@@ -4,6 +4,7 @@ import type { StoreProduct } from "@medusajs/types";
 import Image from "next/image";
 import { type FC, useState } from "react";
 import { cn } from "shared/lib/utils";
+import plantPlaceholder from "../../../../public/image/plant-placholder.svg";
 
 type ProductImageProps = {
   product: StoreProduct;
@@ -16,14 +17,27 @@ export const ProductImage: FC<ProductImageProps> = ({ product }) => {
 
   return (
     <div className="flex flex-col gap-4">
-      <Image
-        src={currentThumbnail}
-        width={620}
-        height={620}
-        alt={`${product.title} thumbnail`}
-        className="aspect-square rounded-xl border"
-      />
-      {product.images?.length && product.images?.length > 1 && (
+      {product.thumbnail ? (
+        <Image
+          src={currentThumbnail}
+          width={620}
+          height={620}
+          alt={`${product.title} thumbnail`}
+          className="aspect-square rounded-xl border"
+        />
+      ) : (
+        <div className="aspect-square rounded-xl border flex justify-center items-center">
+          <Image
+            src={plantPlaceholder.src}
+            width={620}
+            height={620}
+            alt={`${product.title} thumbnail`}
+            className="w-[70%] object-cover translate-x-2"
+          />
+        </div>
+      )}
+
+      {product.images && product.images?.length > 1 && (
         <div className="flex gap-2 justify-center">
           {product.images?.map((image, index) => (
             <Image
