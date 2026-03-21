f22f98bee9868f99754b36d758d7296ce3471fba
revus05 <skninni05@gmail.com>
2026-03-21 14:36:11 +0300

    feat: delete images choose from product page

--- garden-for-you/src/widgets/product/image/ui.tsx
+++ garden-for-you/src/widgets/product/image/ui.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import type { StoreProduct } from "@medusajs/types";
 import Image from "next/image";
-import { type FC, useState } from "react";
-import { cn } from "shared/lib/utils";
+import type { FC } from "react";
 import plantPlaceholder from "../../../../public/image/plant-placholder.svg";
 
 type ProductImageProps = {
@@ -11,49 +8,24 @@ type ProductImageProps = {
 };
 
 export const ProductImage: FC<ProductImageProps> = ({ product }) => {
-  const [currentThumbnail, setCurrentThumbnail] = useState(
-    product.thumbnail || "",
-  );
-
   return (
-    <div className="flex flex-col gap-4">
+    <div className="aspect-square rounded-xl border flex justify-center items-center">
       {product.thumbnail ? (
         <Image
-          src={currentThumbnail}
+          src={product.thumbnail}
           width={620}
           height={620}
           alt={`${product.title} thumbnail`}
-          className="aspect-square rounded-xl border"
+          className="aspect-square rounded-xl"
         />
       ) : (
-        <div className="aspect-square rounded-xl border flex justify-center items-center">
-          <Image
-            src={plantPlaceholder.src}
-            width={620}
-            height={620}
-            alt={`${product.title} thumbnail`}
-            className="w-[70%] object-cover translate-x-2"
-          />
-        </div>
-      )}
-
-      {product.images && product.images?.length > 1 && (
-        <div className="flex gap-2 justify-center">
-          {product.images?.map((image, index) => (
-            <Image
-              key={image.url}
-              src={image.url}
-              width={100}
-              height={100}
-              alt={`${product.title} image ${index}`}
-              className={cn(
-                "aspect-square rounded-lg border",
-                currentThumbnail === image.url && " border-primary",
-              )}
-              onClick={() => setCurrentThumbnail(image.url)}
-            />
-          ))}
-        </div>
+        <Image
+          src={plantPlaceholder.src}
+          width={620}
+          height={620}
+          alt={`${product.title} thumbnail`}
+          className="w-[70%] object-cover translate-x-2"
+        />
       )}
     </div>
   );
